6a578973a3c95d0d077ceec8a0b3c8c63b3e3aa3
storres8 <stevetorres7562@gmail.com>
2019-07-03 23:14:56 -0400

    update.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -17,8 +17,8 @@ export class RecipeItemComponent implements OnInit {
 
   onSelected() {
     // We are emitting a method from our recipe service component inside our
-    // recipe-item component. This from this component we are emitting a specific recipe
-    // that we have selected.
+    // recipe-item component. This from this component we are emitting the specific recipe
+    // that we have selected when clicking from the list of recipes in the DOM.
     this.recipeService.recipeSelected.emit(this.recipe);
   }
 }
