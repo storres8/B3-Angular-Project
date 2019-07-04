de4edf2e56701db3feeed12ae8144d1f3479e4fe
storres8 <stevetorres7562@gmail.com>
2019-07-04 13:09:22 -0400

    shopping list components to pull from shopping-list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,19 @@ export class ShoppingListComponent implements OnInit {
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
+    // this sets ingredients to the basic prefilled array
     this.ingredients = this.shoppingListService.getIngredients();
+
+    // this injected property from our service is subscribed to the ingredientsChanged event.
+    // By subscrbing to the ingredientsChanged event we are reciving what the event is emiting,
+    // everytime there is a new emition of the event. In this case we emit a new array of ingredients
+    // from the shopping-list service whenever we add a new ingridient into the array. This new array
+    // of ingridients is then sent here since we are subscribed and we then set the ingredients
+    // property of this component equal to the newly emmited array of ingredients.
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,19 +1,29 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class ShoppingListService {
-  ingredients = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)];
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10)
+  ];
 
   constructor() {}
 
+  // this is for rendering the first ingridients that we already have. It does not update.
   getIngredients() {
     return this.ingredients.slice();
   }
 
+  // Upon adding a new ingredient we re-copy our ingredients array and send as an event to the
+  // shopping-list component this new updated ingridient array to be rendered. That way our
+  // original array does not get mutated directly.
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
