9047ca190f6c60060feccf76f82819db81cff764
storres8 <stevetorres7562@gmail.com>
2019-07-04 17:31:34 -0400

    updated recipe service to include ingredients.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: "root"
@@ -11,14 +12,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
-      "Another Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "Big Fat Burger",
+      "What else do you need to say?",
+      "https://cdn.sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
