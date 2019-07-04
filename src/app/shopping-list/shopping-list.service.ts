1b8dab81d188c7c66855a1a039ea1d0ccd8ef91e
storres8 <stevetorres7562@gmail.com>
2019-07-04 12:16:36 -0400

    moves ingredients array and adding new ingredients function into a shopping service component.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ShoppingListService {
+  ingredients = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)];
 
-  constructor() { }
+  constructor() {}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
