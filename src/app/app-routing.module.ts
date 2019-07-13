ab0121e6a746fa5a56576ee8d5bcefe3efca0308
storres8 <stevetorres7562@gmail.com>
2019-07-12 21:32:22 -0400

    created and rendered a app-start component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 const appRoutes: Routes = [
   // we all the pathMatch:'full' parameter to the object where we are redirecting b/c we want to make sure
   // that we are mathing a full path that is soley blank.
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
-  { path: "recipes", component: RecipesComponent },
+  {
+    path: "recipes",
+    component: RecipesComponent,
+    children: [{ path: "", component: RecipeStartComponent }]
+  },
   { path: "shopping-list", component: ShoppingListComponent }
 ];
 
