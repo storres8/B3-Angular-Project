39a4939f70aa99636fcb1553efa559b2a225581a
Steve Torres <storres032@stevens-mbp.metro.us.nam.pwcinternal.com>
2019-08-13 16:01:25 -0400

    implemented ngOnDestory in recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html",
   styleUrls: ["./recipe-list.component.css"]
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(
     private recipesService: RecipeService,
@@ -28,6 +30,10 @@ export class RecipeListComponent implements OnInit {
 
     const recipeList = this.recipesService.getRecipes();
     this.recipes = recipeList;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   handleNew() {
